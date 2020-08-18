9aba8b3103b113b0752cc7f0a243ca878b23ffe7
Ayush Gupta <54956547+Ayush-Gupta21@users.noreply.github.com>
2020-08-18 11:00:06 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,4 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const thiscity = urlParams.get('city');
+document.getElementById("mycity").innerHTML = thiscity;
